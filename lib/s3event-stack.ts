7984c20390bece376ab90387451c6a3fb2b1c7a1
AdityaSP <sp.aditya@gmail.com>
2024-05-17 21:30:12 +0530

    Refactor 1: Team creating s3 only

--- lib/s3event-stack.ts
+++ lib/s3event-stack.ts
@@ -6,11 +6,27 @@ export class S3EventStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const alertLamName = cdk.Fn.importValue("alter-lambda-name");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3EventQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    const lam = cdk.aws_lambda.Function.fromFunctionName(this, 'loadlambda', alertLamName)
+    // Second hat : Team member who wants to create an S3 bucket
+    const mys3 = new cdk.aws_s3.Bucket(this, "mys3", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // const eventTypes : cdk.aws_s3.EventType[] = [
+    //   cdk.aws_s3.EventType.OBJECT_CREATED,
+    //   cdk.aws_s3.EventType.OBJECT_REMOVED
+    // ];
+
+    mys3.addEventNotification(
+      cdk.aws_s3.EventType.OBJECT_CREATED,
+      new cdk.aws_s3_notifications.LambdaDestination(lam)
+    );
+    mys3.addEventNotification(
+      cdk.aws_s3.EventType.OBJECT_REMOVED,
+      new cdk.aws_s3_notifications.LambdaDestination(lam)
+    );
   }
 }
